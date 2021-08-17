48cdf17889aae88a4fe2136f37c16eea43c755ea
Your Name <you@example.com>
2021-08-17 16:20:42 +0800

    剥离组件

--- 13-tarbar/src/App.vue
+++ 13-tarbar/src/App.vue
@@ -1,39 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <TabBar>
-      <TarBarItem link="/home" activeColor="deeppink">
-        <img slot="item-icon" alt="" src="./assets/img/tabbar/home.svg">
-        <img slot="item-icon-active" alt="" src="./assets/img/tabbar/home_active.svg">
-        <div slot="item-text">首页</div>
-      </TarBarItem>
-      <TarBarItem link="/category">
-        <img slot="item-icon" alt="" src="./assets/img/tabbar/category.svg">
-        <img slot="item-icon-active" alt="" src="./assets/img/tabbar/category_active.svg">
-        <div slot="item-text">分类</div>
-      </TarBarItem>
-      <TarBarItem link="/cart">
-        <img slot="item-icon" alt="" src="./assets/img/tabbar/shopcart.svg">
-        <img slot="item-icon-active" alt="" src="./assets/img/tabbar/shopcart_active.svg">
-        <div slot="item-text">购物车</div>
-      </TarBarItem>
-      <TarBarItem link="/profile">
-        <img slot="item-icon" alt="" src="./assets/img/tabbar/profile.svg">
-        <img slot="item-icon-active" alt="" src="./assets/img/tabbar/profile_active.svg">
-        <div slot="item-text">我的</div>
-      </TarBarItem>
-    </TabBar>
+    <MainTarBar></MainTarBar>
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar";
-import TarBarItem from "./components/tabbar/TarBarItem";
+import MainTarBar from "./components/tabbar/MainTarBar";
 export default {
   name: 'App',
   components: {
-    TabBar,
-    TarBarItem
+    MainTarBar
   }
 }
 </script>
---
+++ 13-tarbar/src/components/tabbar/MainTarBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <TabBar>
+    <TarBarItem link="/home" activeColor="deeppink">
+      <img slot="item-icon" alt="" src="../../assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" alt="" src="../../assets/img/tabbar/home_active.svg">
+      <div slot="item-text">首页</div>
+    </TarBarItem>
+    <TarBarItem link="/category">
+      <img slot="item-icon" alt="" src="../../assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" alt="" src="../../assets/img/tabbar/category_active.svg">
+      <div slot="item-text">分类</div>
+    </TarBarItem>
+    <TarBarItem link="/cart">
+      <img slot="item-icon" alt="" src="../../assets/img/tabbar/shopcart.svg">
+      <img slot="item-icon-active" alt="" src="../../assets/img/tabbar/shopcart_active.svg">
+      <div slot="item-text">购物车</div>
+    </TarBarItem>
+    <TarBarItem link="/profile">
+      <img slot="item-icon" alt="" src="../../assets/img/tabbar/profile.svg">
+      <img slot="item-icon-active" alt="" src="../../assets/img/tabbar/profile_active.svg">
+      <div slot="item-text">我的</div>
+    </TarBarItem>
+  </TabBar>
+</template>
+
+<script>
+import TabBar from "./TabBar";
+import TarBarItem from "./TarBarItem";
+export default {
+  name: "MainTarBar",
+  components: {
+    TabBar,
+    TarBarItem
+  }
+}
+</script>
+
+<style scoped>
+  @import "../../assets/css/base.css";
+</style>
